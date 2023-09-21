27fdb18da1d0404f0ebcf50c33ca56ac5540ecea
watzerfeiss <p.a.pesterev@gmail.com>
2023-09-22 01:11:51 +0400

    add basic layout to app

--- routes/_app.tsx
+++ routes/_app.tsx
@@ -1,15 +1,24 @@
 import { AppProps } from "$fresh/server.ts";
+import { tw } from "twind";
 
 export default function App({ Component }: AppProps) {
   return (
-    <html>
+    <html lang="en">
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <title>planning poker</title>
       </head>
-      <body class="bg-[#facade]">
-        <Component />
+      <body class={tw`bg-[whitesmoke] min-h-screen flex flex-col`}>
+        <header>
+          <a href="/">Planning poker</a>
+        </header>
+        <main class="flex-grow mx-auto px-4 py-8 max-w-2xl">
+          <Component />
+        </main>
+        <footer>
+          © watzerfeiss 2021
+        </footer>
       </body>
     </html>
   );
