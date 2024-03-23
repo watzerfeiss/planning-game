6b680ca7e61046f51641b13767666ffd26b745bb
watzerfeiss <p.a.pesterev@gmail.com>
2024-03-23 18:52:21 +0400

    improve appearance of header with logout link on smaller screens

--- routes/_app.tsx
+++ routes/_app.tsx
@@ -15,26 +15,30 @@ export default defineApp<CtxState>((req, ctx) => {
       </head>
 
       <body class="text-gray-800 bg-slate-50 min-h-screen flex flex-col">
-        <header class="p-4 flex justify-between items-center gap-1 bg-white shadow-sm">
+        <header class="py-2 px-4 flex justify-between items-baseline gap-4 bg-white shadow-sm">
           <div class="flex flex-col">
             <a href="/" class="text-xl text-slate-600 font-semibold">
               Projection holdem
             </a>
-            <span class="text-xs text-slate-400">
+            <span class="text-xs text-slate-400 max-md:hidden">
               cause "planning poker" is copyrighted or something idk
             </span>
           </div>
+
           {user && (
-            <div class="divide-x flex gap-2">
+            <div class="text-right md:divide-x flex md:gap-2 max-md:flex-col">
               <span>
-                Logged in as <strong class="font-semibold">{user.name}</strong>
+                <span class="max-md:hidden">{"Logged in as "}</span>
+                <strong class="font-semibold">{user.name}</strong>
               </span>
-              <a
-                href="/api/logout"
-                class="pl-2 text-slate-400 underline hover:no-underline"
-              >
-                Log out
-              </a>
+              <span class="md:pl-2 flex-shrink-0">
+                <a
+                  href="/api/logout"
+                  class="text-slate-400 underline hover:no-underline"
+                >
+                  Log out
+                </a>
+              </span>
             </div>
           )}
         </header>
@@ -45,14 +49,14 @@ export default defineApp<CtxState>((req, ctx) => {
           </div>
         </main>
 
-        <footer class="p-4 flex justify-between gap-1">
-          <div class="ml-auto flex flex-col items-end">
+        <footer class="py-2 px-4 flex justify-between gap-1">
+          <div class="ml-auto flex flex-col items-end text-end">
             <span class="text-sm text-gray-400">
               © {new Date().getFullYear()} watzerfeiss
             </span>
             <span class="text-xs text-gray-400">
-              Site icon courtesy of Twemoji, © 2023 X Corp. (I guess) under
-              CC-BY-4.0
+              Site icon courtesy of Twemoji, © {new Date().getFullYear()}{" "}
+              X Corp. (I guess) under CC-BY-4.0
             </span>
           </div>
         </footer>
